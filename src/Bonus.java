be735fc85996a60dc02d448ad2095bc34b413e94
Sergey <tel.cromax@gmail.com>
2021-07-19 18:18:50 +1100

    Update App

--- src/Bonus.java
+++ src/Bonus.java
@@ -1,16 +1,16 @@
 public class Bonus {
     public static void main(String[] args) {
-        int account = 200;
-        int amount = 1790;
-        int bonus;
+        int account = 290;
+        int amount = 1890;
+        int totalBonus = 1;
 
         account += amount;
+        int bonus;
         if (amount > 1000) {
             bonus = (amount - 1000) / 100;
-        } else {
-            bonus = 0;
+            totalBonus += bonus;
         }
         System.out.println("Счет: " + account);
-        System.out.println("Бонус: " + bonus);
+        System.out.println("Бонус: " + totalBonus);
     }
 }
